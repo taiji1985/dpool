5a15f4e238e7d54e427a79427d235f7e16f0db13
yangtf <ytf_003@126.com>
2023-10-16 21:35:18 +0800

    add test code

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <chrono>
 #include <mutex>
+#include <queue>
 
 using namespace dpool;
 
@@ -37,16 +38,16 @@ void monitor(const ThreadPool &pool, int seconds)
     }
 }
 
-int main(int argc, char *argv[])
+int main_old(int argc, char *argv[])
 {
     // max threads number is 100
     ThreadPool pool(100); 
 
     // monitoring threads number for 13 seconds    
-    pool.submit(monitor, std::ref(pool), 13);
+    pool.submit(monitor, std::ref(pool), 4);
     
     // submit 100 tasks
-    for (int taskId = 0; taskId < 100; ++taskId)
+    for (int taskId = 0; taskId < 10; ++taskId)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         pool.submit(task, taskId);
@@ -54,3 +55,81 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
+
+
+int compute(int a, int b)
+{
+	int r = rand()%1400+1000;
+	std::cout<<"compute start "<< a<<"+"<<b <<",sleep = "<<r<<std::endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(r));
+	std::cout<<"compute end "<< a<<"+"<<b<<std::endl;
+    return a + b;
+}
+
+using namespace std; 
+
+template <typename T>
+class safe_queue {
+public:
+    void push(const T& value) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push(value);
+    }
+
+    T pop() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        T value = queue_.front();
+        queue_.pop();
+        return value;
+    }
+
+    bool empty() const {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return queue_.empty();
+    }
+
+private:
+    std::queue<T> queue_;
+    mutable std::mutex mutex_;
+};
+
+
+safe_queue<std::shared_future<int>> q;
+
+void my_sleep(int ms){
+	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+void getter(){
+	cout<<"start get "<<endl;
+	while(true){
+		
+		while(q.empty()){
+			my_sleep(100);
+		}
+		std::shared_future<int> f = q.pop();
+		int v = f.get();
+		std::cout<<"-----------------"<<v<<std::endl;
+	}
+}
+
+int main()
+{
+    // 设置最大线程数为 10
+    dpool::ThreadPool pool(30);
+    
+    pool.submit(getter);
+    
+    for(int i=0;i<100;i++){
+    	my_sleep(30);
+    	std::shared_future<int> fut = pool.submit(compute, i, 0);    
+		q.push(fut);
+	} 
+	
+    return 0;
+}
+
+
+
+
